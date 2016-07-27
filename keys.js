ea5c28319d2f0720308b53ba72f0a1b5d1dec4fd
Angel Trinh <trinh.angel@gmail.com>
2016-07-27 09:50:06 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '40dqesL9DzcVZdNrmeKXgnGPb',
-  consumer_secret: '0tj0Lr4tHLQKQqwSu6br1fdKJiAgqr3FfZmZ60hi8N1p1FiGkz',
-  access_token_key: '161861111-duBS6hL4Ivdd2ZSj3HqXoQwKJ3Ds9qUOlg1SGfjK',
-  access_token_secret: 'O8uPLTFuyhJ1aSweHWeiUMdY5UbMmBRhV3SVnHeQAsyhb',
-}
